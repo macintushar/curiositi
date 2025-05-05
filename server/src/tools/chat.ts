a6e2a7e6787cbe474b46588a51ee7b470619c962
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-05 14:57:26 +0530

    feat: added openrouter support

--- server/src/tools/chat.ts
+++ server/src/tools/chat.ts
@@ -1,9 +1,20 @@
-import { OLLAMA_BASE_URL } from "@/constants";
+import { OLLAMA_BASE_URL, OPENROUTER_API_KEY } from "@/constants";
 import { ChatOllama } from "@langchain/ollama";
+import { ChatOpenAI } from "@langchain/openai";
 
 export const OllamaChat = (model: string) =>
-	new ChatOllama({
-		baseUrl: OLLAMA_BASE_URL,
-		model,
-		temperature: 0.7,
-	});
+  new ChatOllama({
+    baseUrl: OLLAMA_BASE_URL,
+    model,
+    temperature: 0.4,
+  });
+
+export const OpenRouterChat = (model: string) =>
+  new ChatOpenAI({
+    model: model,
+    temperature: 0.4,
+    apiKey: OPENROUTER_API_KEY,
+    configuration: {
+      baseURL: "https://openrouter.ai/api/v1",
+    },
+  });
