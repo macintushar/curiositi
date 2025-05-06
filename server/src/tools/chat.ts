977ae177ae67023801dd5666c76073fee56bc3f1
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-06 15:34:25 +0530

    refactor: moved from LangChain Ollama to Ollama-JS and OpenAI SDK

--- server/src/tools/chat.ts
+++ server/src/tools/chat.ts
@@ -1,24 +1,21 @@
 import { HOST, OLLAMA_BASE_URL, OPENROUTER_API_KEY } from "@/constants";
-import { ChatOllama } from "@langchain/ollama";
-import { ChatOpenAI } from "@langchain/openai";
 
-export const OllamaChat = (model: string) =>
-  new ChatOllama({
-    baseUrl: OLLAMA_BASE_URL,
-    model,
-    temperature: 0.4,
-  });
+import { Ollama } from "ollama";
+import OpenAI from "openai";
 
-export const OpenRouterChat = (model: string) =>
-  new ChatOpenAI({
-    model: model,
-    temperature: 0.4,
-    apiKey: OPENROUTER_API_KEY,
-    configuration: {
-      baseURL: "https://openrouter.ai/api/v1",
-      defaultHeaders: {
-        "HTTP-Referer": HOST,
-        "X-Title": "Curiositi",
-      },
-    },
-  });
+export const ollama = new Ollama({
+  host: OLLAMA_BASE_URL,
+});
+
+export const openai = new OpenAI({
+  apiKey: OPENROUTER_API_KEY,
+});
+
+export const openrouter = new OpenAI({
+  apiKey: OPENROUTER_API_KEY,
+  baseURL: "https://openrouter.ai/api/v1",
+  defaultHeaders: {
+    "HTTP-Referer": HOST,
+    "X-Title": "Curiositi",
+  },
+});
