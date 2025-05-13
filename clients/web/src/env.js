f2ddf32a306c8da21fba1212282915a12138cd13
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-13 19:00:07 +0530

    feat: added metadata for better SEO

--- clients/web/src/env.js
+++ clients/web/src/env.js
@@ -9,6 +9,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     SERVER_URL: z.string().url(),
+    BASE_URL: z.string().url(),
   },
 
   /**
@@ -27,6 +28,7 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     SERVER_URL: process.env.SERVER_URL,
+    BASE_URL: process.env.BASE_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
