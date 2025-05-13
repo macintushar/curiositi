ea8c02c2bb46c47ed5f845f2e84b90d1ebcecbf5
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-13 19:15:36 +0530

    fix: lint error

--- clients/web/src/constants.ts
+++ clients/web/src/constants.ts
@@ -3,5 +3,5 @@ import { env } from "./env";
 export const webMetaData = {
   siteName: "Curiositi",
   description: "Curiositi is an AI-powered search engine for your data.",
-  url: env.BASE_URL as string,
+  url: env.BASE_URL,
 };
