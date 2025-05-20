5a91b9430f3b58c62072d0013870227f47bb6a9c
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-20 15:25:32 +0530

    chore: simplified FAQs page

--- clients/web/src/app/faq/page.tsx
+++ clients/web/src/app/faq/page.tsx
@@ -9,24 +9,20 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Section } from "@/components/landing/section";
 
 export default function FAQPage() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center">
       <Header />
       <main className="w-full flex-1 pt-4">
-        <section className="w-full py-20 md:py-32">
+        <Section
+          id="features"
+          className="bg-secondary/50"
+          title="Frequently Asked Questions"
+          subtitle="Curiositi combines the best of AI language models with your data and the latest information from the web."
+        >
           <div className="mx-auto max-w-3xl space-y-2 px-4 md:px-6">
-            <div className="flex flex-col gap-2 text-center">
-              <h1 className="text-gradient font-serif text-4xl font-medium tracking-tight md:text-5xl">
-                Frequently Asked Questions
-              </h1>
-              <p className="text-muted-foreground text-md max-w-2xl">
-                Curiositi combines your documents with live web data to deliver
-                context-aware answers using advanced AI and Retrieval-Augmented
-                Generation.
-              </p>
-            </div>
             <div className="mt-6">
               <Accordion type="multiple">
                 {faqs.map((faq, idx) => (
@@ -40,7 +36,7 @@ export default function FAQPage() {
               </Accordion>
             </div>
           </div>
-        </section>
+        </Section>
       </main>
       <Footer />
     </div>
