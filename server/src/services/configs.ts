e7c489133bb2d5564bafd52bc4790d755f459537
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-05-21 00:50:07 +0530

    feat: added Anthropic support

--- server/src/services/configs.ts
+++ server/src/services/configs.ts
@@ -100,6 +100,14 @@ export const models: Provider[] = [
       },
     ],
   },
+  {
+    name: LLM_PROVIDERS.ANTHROPIC,
+    models: [
+      "claude-3-7-sonnet-20250219",
+      "claude-3-5-sonnet-20241022",
+      "claude-3-5-sonnet-20240620",
+    ],
+  },
 ];
 
 // Cache for Ollama models
