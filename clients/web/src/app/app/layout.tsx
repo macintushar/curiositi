fa76c2c424681fa9e30895f69164d77a92f71984
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-06-05 23:22:33 +0530

    refactor: simplify error handling in AppLayout by removing unused data fetching for files, spaces, and configs

--- clients/web/src/app/app/layout.tsx
+++ clients/web/src/app/app/layout.tsx
@@ -6,23 +6,12 @@ import Section from "@/components/app/section";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 import { getThreads } from "@/services/chats";
-import { getSpaces } from "@/services/spaces";
-import { getConfigs } from "@/services/configs";
-import { getUsersFiles } from "@/services/files";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const { data: threads, error: threadsError } = use(getThreads());
-  const { data: files, error: filesError } = use(getUsersFiles());
-  const { data: spaces, error: spacesError } = use(getSpaces());
-  const { data: configs, error: configsError } = use(getConfigs());
 
-  if (threadsError || filesError || spacesError || configsError) {
-    return (
-      <div>
-        Error: {threadsError?.message} {filesError?.message}{" "}
-        {spacesError?.message} {configsError?.message}
-      </div>
-    );
+  if (threadsError) {
+    return <div>Error: {threadsError?.message}</div>;
   }
 
   return (
