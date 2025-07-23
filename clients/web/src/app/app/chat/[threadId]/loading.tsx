4ce2b7449c002f9b86e9ec6cc206e079a965cef2
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-07-23 18:29:26 +0530

    refactor: enhanced loading state for chat thread

--- clients/web/src/app/app/chat/[threadId]/loading.tsx
+++ clients/web/src/app/app/chat/[threadId]/loading.tsx
@@ -12,10 +12,19 @@ function TextSkeleton({ className }: { className?: string }) {
   return (
     <div
       className={cn(
-        "animate keyframes-[shimmer-text] h-3 w-full rounded-sm bg-black dark:bg-white",
+        "h-3 w-full animate-pulse rounded-sm bg-black dark:bg-white",
         className,
       )}
     ></div>
+  );
+}
+
+function RelatedSkeleton() {
+  return (
+    <div className="flex items-center justify-between">
+      <TextSkeleton className="w-1/4" />
+      <TablerIcon Icon={IconPlus} className="animate-pulse" />
+    </div>
   );
 }
 
@@ -23,29 +32,31 @@ export default function ThreadLoading() {
   return (
     <div className="flex h-full flex-col items-center justify-between gap-2 px-2 py-2">
       <div className="h-full w-full max-w-4xl flex-1 overflow-y-auto">
-        <div className="mb-16 flex h-full flex-col justify-between pb-16">
+        <div className="mb-16 flex h-full flex-col space-y-32 pb-16">
           <div className="flex flex-col gap-4">
-            <div className="bg-brand keyframes-[shimmer-text] h-8 w-1/3 opacity-80"></div>
+            <div className="bg-brand h-8 w-1/3 animate-pulse opacity-80"></div>
+            {/* title skeleton */}
             <Separator className="bg-muted-foreground" />
             <TextSkeleton className="w-full" />
             <TextSkeleton className="w-full" />
             <TextSkeleton className="w-3/4" />
             <TextSkeleton className="w-2/3" />
-            <div className="h-12" />
+            <div className="h-1" /> {/* breakpoint skeleton */}
             <TextSkeleton className="w-full" />
             <TextSkeleton className="w-full" />
+            <Separator className="bg-muted-foreground mt-8" />
           </div>
           <div className="flex flex-col gap-4">
-            <div className="bg-brand keyframes-[shimmer-text] h-8 w-24 opacity-80"></div>
+            {/* related skeleton */}
+            <div className="bg-brand h-6 w-24 animate-pulse opacity-80"></div>
+            {/* related title skeleton */}
             <Separator className="bg-muted-foreground" />
-            <div className="flex items-center justify-between">
-              <TextSkeleton className="w-1/4" />
-              <TablerIcon Icon={IconPlus} />
-            </div>
+            {/* related item skeleton */}
+            <RelatedSkeleton />
             <Separator className="bg-muted-foreground" />
-            <TextSkeleton className="w-1/4" />
+            <RelatedSkeleton />
             <Separator className="bg-muted-foreground" />
-            <TextSkeleton className="w-1/4" />
+            <RelatedSkeleton />
           </div>
         </div>
       </div>
