f489e7ca14fdef0cdb7803a93aa5e72b3bb0b806
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-07-23 20:18:09 +0530

    chore: temp logging

--- clients/web/src/middleware.ts
+++ clients/web/src/middleware.ts
@@ -1,10 +1,13 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
+import { env } from "./env";
 
 export async function middleware(request: NextRequest) {
   const sessionCookie = getSessionCookie(request);
 
   if (!sessionCookie) {
+    console.log(env.NODE_ENV);
+    console.log("No session cookie found");
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
