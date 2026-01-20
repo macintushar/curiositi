ea6248d93222f703f2daad216abf29406f06204a
Tushar Selvakumar <tusharkumar91111@gmail.com>
2026-01-21 01:21:29 +0800

    fix(platform): file icon

--- apps/platform/src/components/file-icon.tsx
+++ apps/platform/src/components/file-icon.tsx
@@ -1,4 +1,4 @@
-import { IconFile, IconFileTypePdf } from "@tabler/icons-react";
+import { IconFile, IconFileText, IconFileTypePdf } from "@tabler/icons-react";
 import { TEXT_FILE_TYPES, PDF_TYPE } from "@curiositi/share/constants";
 
 type FileIconProps = {
@@ -7,14 +7,13 @@ type FileIconProps = {
 };
 
 export default function FileIcon({ type, className }: FileIconProps) {
-	if (!TEXT_FILE_TYPES.includes(type) && type !== PDF_TYPE) {
-		return <IconFile className={className} />;
+	if (type === PDF_TYPE) {
+		return <IconFileTypePdf className={className} />;
 	}
 
-	switch (type) {
-		case "text/plain":
-			return <IconFileTypePdf className={className} />;
-		default:
-			return <IconFileTypePdf className={className} />;
+	if (TEXT_FILE_TYPES.includes(type)) {
+		return <IconFileText className={className} />;
 	}
+
+	return <IconFile className={className} />;
 }
