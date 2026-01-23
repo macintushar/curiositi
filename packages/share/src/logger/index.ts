a6ab936d37a898efbd0e0d1a65fdafc0e73cfdd0
Tushar Selvakumar <tusharkumar91111@gmail.com>
2026-01-23 20:15:13 +0800

    feat(logger): added warn type

--- packages/share/src/logger/index.ts
+++ packages/share/src/logger/index.ts
@@ -1,5 +1,6 @@
 const logger = {
 	info: (msg: string, obj?: unknown) => console.info(`[INFO] ${msg}`, obj),
+	warn: (msg: string, obj?: unknown) => console.warn(`[WARN] ${msg}`, obj),
 	error: (msg: unknown, obj?: unknown) => console.error(`[ERROR] ${msg}`, obj),
 	debug: (msg: string, obj?: unknown) => console.debug(`[DEBUG] ${msg}`, obj),
 };
