d8d93320bd45e8e268d46777487e7dda3c3f1005
Tushar Selvakumar <tusharkumar91111@gmail.com>
2026-02-13 20:33:40 +0530

    feat(www): added astro website

---
+++ apps/www/astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+
+export default defineConfig({
+	site: "https://curiositi.xyz",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [
+		starlight({
+			title: "Curiositi Docs",
+			customCss: ["./src/styles/global.css"],
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/curiositi",
+				},
+			],
+			sidebar: [
+				{
+					label: "Welcome to Curiositi",
+					link: "/docs",
+				},
+				{
+					label: "Reference",
+					autogenerate: { directory: "docs/reference" },
+				},
+			],
+		}),
+		sitemap(),
+		icon(),
+	],
+});
